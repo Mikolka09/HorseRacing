601023a0bb515cee3f19aea8c8c99fd253150175
Микола Матіос <mikolka09@gmail.com>
2021-02-24 17:15:01 +0200

    Добавьте файлы проекта.

---
+++ HorseRacing/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace HorseRacing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.progressBarHorse1 = new System.Windows.Forms.ProgressBar();
+            this.progressBarHorse2 = new System.Windows.Forms.ProgressBar();
+            this.progressBarHorse3 = new System.Windows.Forms.ProgressBar();
+            this.progressBarHorse4 = new System.Windows.Forms.ProgressBar();
+            this.progressBarHorse5 = new System.Windows.Forms.ProgressBar();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonResStart = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(152, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "КОННЫЕ СКАЧКИ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(341, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ИППОДРОМ";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(458, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 37);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "СТАРТ";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // progressBarHorse1
+            // 
+            this.progressBarHorse1.Location = new System.Drawing.Point(87, 118);
+            this.progressBarHorse1.Name = "progressBarHorse1";
+            this.progressBarHorse1.Size = new System.Drawing.Size(596, 34);
+            this.progressBarHorse1.TabIndex = 3;
+            // 
+            // progressBarHorse2
+            // 
+            this.progressBarHorse2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.progressBarHorse2.Location = new System.Drawing.Point(87, 191);
+            this.progressBarHorse2.Name = "progressBarHorse2";
+            this.progressBarHorse2.Size = new System.Drawing.Size(596, 34);
+            this.progressBarHorse2.TabIndex = 4;
+            // 
+            // progressBarHorse3
+            // 
+            this.progressBarHorse3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.progressBarHorse3.Location = new System.Drawing.Point(87, 260);
+            this.progressBarHorse3.Name = "progressBarHorse3";
+            this.progressBarHorse3.Size = new System.Drawing.Size(596, 34);
+            this.progressBarHorse3.TabIndex = 5;
+            // 
+            // progressBarHorse4
+            // 
+            this.progressBarHorse4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.progressBarHorse4.Location = new System.Drawing.Point(87, 333);
+            this.progressBarHorse4.Name = "progressBarHorse4";
+            this.progressBarHorse4.Size = new System.Drawing.Size(596, 34);
+            this.progressBarHorse4.TabIndex = 6;
+            // 
+            // progressBarHorse5
+            // 
+            this.progressBarHorse5.ForeColor = System.Drawing.Color.Fuchsia;
+            this.progressBarHorse5.Location = new System.Drawing.Point(87, 402);
+            this.progressBarHorse5.Name = "progressBarHorse5";
+            this.progressBarHorse5.Size = new System.Drawing.Size(596, 34);
+            this.progressBarHorse5.TabIndex = 7;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.Location = new System.Drawing.Point(675, 12);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(84, 36);
+            this.buttonExit.TabIndex = 8;
+            this.buttonExit.Text = "ВЫХОД";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "СТАРТ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(701, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ФИНИШ";
+            // 
+            // buttonResStart
+            // 
+            this.buttonResStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.buttonResStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonResStart.Location = new System.Drawing.Point(557, 12);
+            this.buttonResStart.Name = "buttonResStart";
+            this.buttonResStart.Size = new System.Drawing.Size(97, 36);
+            this.buttonResStart.TabIndex = 11;
+            this.buttonResStart.Text = "РЕСТАРТ";
+            this.buttonResStart.UseVisualStyleBackColor = false;
+            this.buttonResStart.Click += new System.EventHandler(this.buttonResStart_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(87, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Лошадь №1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(87, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Лошадь №2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(87, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Лошадь №3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(87, 313);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Лошадь №4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(87, 382);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Лошадь №5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 462);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonResStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.progressBarHorse5);
+            this.Controls.Add(this.progressBarHorse4);
+            this.Controls.Add(this.progressBarHorse3);
+            this.Controls.Add(this.progressBarHorse2);
+            this.Controls.Add(this.progressBarHorse1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "КОННЫЕ СКАЧКИ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ProgressBar progressBarHorse1;
+        private System.Windows.Forms.ProgressBar progressBarHorse2;
+        private System.Windows.Forms.ProgressBar progressBarHorse3;
+        private System.Windows.Forms.ProgressBar progressBarHorse4;
+        private System.Windows.Forms.ProgressBar progressBarHorse5;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonResStart;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ HorseRacing/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace HorseRacing
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+        public int horse1 = 1;
+        public int horse2 = 2;
+        public int horse3 = 3;
+        public int horse4 = 4;
+        public int horse5 = 5;
+        public List<int> listFinish;
+        public Thread horse_1;
+        public Thread horse_2;
+        public Thread horse_3;
+        public Thread horse_4;
+        public Thread horse_5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Color RandColor()
+        {
+            Random randGen = new Random();
+            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            KnownColor randColorName = names[randGen.Next(names.Length)];
+            Color randomColor = Color.FromKnownColor(randColorName);
+            Thread.Sleep(50);
+            return randomColor;
+        }
+
+        public void Horse_1(object obj)
+        {
+            try
+            {
+                ProgressBar bar = (ProgressBar)obj;
+                int i = 0;
+                bool ex = true;
+                while (ex)
+                {
+                    i = 0;
+                    i = rand.Next(1, 20);
+                    if (bar.InvokeRequired)
+                        bar.Invoke(new Action(() => bar.Increment(i)));
+                    if (bar.Value == bar.Maximum)
+                    {
+                        ex = false;
+                        listFinish.Add(horse1);
+                        if (listFinish.Count == 5)
+                            PrintFinish(listFinish);
+                    }
+                    else
+                        ex = true;
+                    Thread.Sleep(500);
+                }
+            }
+            finally { }
+
+        }
+
+        public void Horse_2(object obj)
+        {
+            try
+            {
+                ProgressBar bar = (ProgressBar)obj;
+                int i = 0;
+                bool ex = true;
+                while (ex)
+                {
+                    i = rand.Next(1, 20);
+                    if (bar.InvokeRequired)
+                        bar.Invoke(new Action(() => bar.Increment(i)));
+                    if (bar.Value == bar.Maximum)
+                    {
+                        ex = false;
+                        listFinish.Add(horse2);
+                        if (listFinish.Count == 5)
+                            PrintFinish(listFinish);
+                    }
+                    Thread.Sleep(500);
+                }
+            }
+            finally { }
+        }
+
+        public void Horse_3(object obj)
+        {
+            try
+            {
+                ProgressBar bar = (ProgressBar)obj;
+                int i = 0;
+                bool ex = true;
+                while (ex)
+                {
+                    i = rand.Next(1, 20);
+                    if (bar.InvokeRequired)
+                        bar.Invoke(new Action(() => bar.Increment(i)));
+                    if (bar.Value == bar.Maximum)
+                    {
+                        ex = false;
+                        listFinish.Add(horse3);
+                        if (listFinish.Count == 5)
+                            PrintFinish(listFinish);
+                    }
+                    Thread.Sleep(500);
+                }
+            }
+            finally { }
+        }
+
+        public void Horse_4(object obj)
+        {
+            try
+            {
+                ProgressBar bar = (ProgressBar)obj;
+                int i = 0;
+                bool ex = true;
+                while (ex)
+                {
+                    i = rand.Next(1, 20);
+                    if (bar.InvokeRequired)
+                        bar.Invoke(new Action(() => bar.Increment(i)));
+                    if (bar.Value == bar.Maximum)
+                    {
+                        ex = false;
+                        listFinish.Add(horse4);
+                        if (listFinish.Count == 5)
+                            PrintFinish(listFinish);
+                    }
+                    Thread.Sleep(500);
+                }
+            }
+            finally { }
+        }
+
+        public void Horse_5(object obj)
+        {
+            try
+            {
+                ProgressBar bar = (ProgressBar)obj;
+                int i = 0;
+                bool ex = true;
+                while (ex)
+                {
+                    i = rand.Next(1, 20);
+                    if (bar.InvokeRequired)
+                        bar.Invoke(new Action(() => bar.Increment(i)));
+                    if (bar.Value == bar.Maximum)
+                    {
+                        ex = false;
+                        listFinish.Add(horse5);
+                        if (listFinish.Count == 5)
+                            PrintFinish(listFinish);
+                    }
+                    Thread.Sleep(500);
+                }
+            }
+            finally { }
+        }
+
+        public void StartProgram()
+        {
+            rand = new Random();
+            listFinish = new List<int>();
+
+            progressBarHorse1.ForeColor = RandColor();
+            progressBarHorse1.Style = ProgressBarStyle.Continuous;
+
+            progressBarHorse2.ForeColor = RandColor();
+            progressBarHorse2.Style = ProgressBarStyle.Continuous;
+
+            progressBarHorse3.ForeColor = RandColor();
+            progressBarHorse3.Style = ProgressBarStyle.Continuous;
+
+            progressBarHorse4.ForeColor = RandColor();
+            progressBarHorse4.Style = ProgressBarStyle.Continuous;
+
+            progressBarHorse5.ForeColor = RandColor();
+            progressBarHorse5.Style = ProgressBarStyle.Continuous;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartProgram();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = false;
+            horse_1 = new Thread(Horse_1);
+            horse_1.IsBackground = true;
+            horse_1.Start(progressBarHorse1);
+
+            horse_2 = new Thread(Horse_2);
+            horse_2.IsBackground = true;
+            horse_2.Start(progressBarHorse2);
+
+            horse_3 = new Thread(Horse_3);
+            horse_3.IsBackground = true;
+            horse_3.Start(progressBarHorse3);
+
+            horse_4 = new Thread(Horse_4);
+            horse_4.IsBackground = true;
+            horse_4.Start(progressBarHorse4);
+
+            horse_5 = new Thread(Horse_5);
+            horse_5.IsBackground = true;
+            horse_5.Start(progressBarHorse5);
+
+
+        }
+
+        public string Position(int hr)
+        {
+            switch (hr)
+            {
+                case 1:
+                    return "Первая Лошадь";
+                case 2:
+                    return "Вторая Лошадь";
+                case 3:
+                    return "Третья Лошадь";
+                case 4:
+                    return "Четвертая Лошадь";
+                case 5:
+                    return "Пятая Лошадь";
+                default:
+                    return "";
+            }
+        }
+
+        public void PrintFinish(List<int> list)
+        {
+            MessageBox.Show($"Конные скачки ПОБЕДИТЕЛИ:\n\n" +
+                $" 1. {Position(list[0])}\n 2. {Position(list[1])}\n 3. {Position(list[2])}\n " +
+                $"4. {Position(list[3])}\n 5. {Position(list[4])}", "ТАБЛИЦА РЕЗУЛЬТАТОВ",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonResStart_Click(object sender, EventArgs e)
+        {
+            foreach (Control item in this.Controls)
+            {
+                if ((item as ProgressBar) == item)
+                    (item as ProgressBar).Value = 0;
+            }
+            if (horse_1 != null)
+                horse_1.Abort();
+            if (horse_2 != null)
+                horse_2.Abort();
+            if (horse_3 != null)
+                horse_3.Abort();
+            if (horse_4 != null)
+                horse_4.Abort();
+            if (horse_5 != null)
+                horse_5.Abort();
+            buttonStart.Enabled = true;
+            StartProgram();
+        }
+    }
+}
